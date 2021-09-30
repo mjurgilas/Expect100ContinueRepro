891a63fae42c1ea8f3e4c9e93a3b3f1402f2b393
Martynas Jurgilas <m.jurgilas@hyarchis.com>
2021-09-30 14:13:10 +0300

    initial commit

---
+++ ExpectResponseTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpectResponseTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(FileWrapper), (int)HttpStatusCode.Created)]
+        public IActionResult UploadFile([FromForm] FileWrapper file)
+        {
+            Console.WriteLine("File received");
+            string message = "FileReceived";
+            return Ok(message);
+        }
+    }
+}
---
+++ ExpectResponseTest/FileWrapper.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ExpectResponseTest
+{
+    public class FileWrapper
+    {
+        public IFormFile FormFile { get; set; }
+    }
+}
